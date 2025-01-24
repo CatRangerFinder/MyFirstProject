42f1f6ac30977dcb0c0d2f8a81bc769cef153444
CatRangerFinder <dunham_jack@yahoo.com>
2025-01-24 20:04:12 +0000

    GUI doesn't run for some reason but good for now

---
+++ Gui.py
@@ -0,0 +1,21 @@
+
+
+
+#some reason this doesen't work
+
+
+
+
+
+#import tkinter
+#import customtkinter
+
+#system settings
+#customtkinter.set_appearance_mode("system")
+#customtkinter.set_default_color_theme("blue")
+
+#app frame
+#app = customtkinter.CTk()
+#app.geometry("720x480")
+#app.title("test")
+
